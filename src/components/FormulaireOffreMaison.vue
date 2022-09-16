cc72ff2ce82aee43854ed52020e504f6cfdca413
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-09-16 10:45:29 +0200

    Avancement tp - formkit

---
+++ src/components/FormulaireOffreMaison.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    import { ref } from "@vue/reactivity";
+    import cardoffre from "./card.vue";
+
+    // On fait une variable réactive qui réactive qui réference les données
+    // ATTENTION : faire une Ref pas une Reactive car :
+    // c'est l'objet qui doit être réactif, pas ses props
+    const maison = ref({nom:"test", price:34, favoris:true, txt:"produit de test", nbbath:2, nbsize:"test m²", img:"/public/card.jpg"});
+</script>
+
+<template>
+    <div>
+        <div class="p-2">
+            <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
+            <!-- Optionnel on affiche les données -->
+            <cardoffre v-bind="maison" />
+        </div>
+        <div class="p-2">
+            <!-- On passe la "ref" à Formkit -->
+            <Formkit type="form" v-model="maison">
+                <Formkit name="nom" label="nom" />
+                <Formkit name="price" label="prix" type="number"/>
+                <Formkit name="favoris" label="mettre en valeur" type="checkbox"/>
+            </Formkit>
+        </div>
+    </div>
+</template>
---
+++ src/pages/edit/new.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import cardoffre from "../../components/card.vue";
+    import offremaison from "../../components/FormulaireOffreMaison.vue"
+
+    const maisonex = {nom:"test", price:34, favoris:false, txt:"produit de test", nbbath:2, nbsize:"test m²", img:"/public/card.jpg"};
+</script>
+
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page New</h1>
+
+        <cardoffre class="w-1/3" v-bind="maisonex"/>
+
+        <offremaison class="w-1/2"/>
+    </div>
+    </template>
